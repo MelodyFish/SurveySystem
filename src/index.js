c541dd39b16e9df5c7faa2c9964274f26ed56ffd
yuguoqiang <yuguoqiang@hygon.cn>
2024-03-18 11:16:31 +0800

    feat: 修改icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import App from './App';
@@ -11,6 +12,7 @@ root.render(
   <Router>
     <ConfigProvider locale={zhCN}>
       <App />
+      <SpeedInsights />
     </ConfigProvider>
   </Router>
 )
